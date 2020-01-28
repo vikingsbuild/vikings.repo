a9ba11c20c9aad6bc2497470ea2f492f7997ea42
vikingsbuild <37523732+vikingsbuild@users.noreply.github.com>
2020-01-27 22:39:01 -0300

    att netfree

---
+++ plugin.video.DejaVu/_Edit.py
@@ -0,0 +1,4 @@
+import xbmcaddon
+import base64
+MainBase = base64.b64decode ('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0Zsb3lkNjk2OS9ESlYvbWFzdGVyL0RKVi50eHQ=')
+addon = xbmcaddon.Addon('plugin.video.DejaVu')
