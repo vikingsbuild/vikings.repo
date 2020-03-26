a07a82d3109182a1e0321816e26148310a7aa17c
vikingsbuild <37523732+vikingsbuild@users.noreply.github.com>
2020-03-26 14:38:13 -0300

    update brasilfull

---
+++ plugin.video.brasilfull/extract.py
@@ -0,0 +1,34 @@
+#import zfile as zipfile
+import zipfile
+#from zipfile import ZipFile
+
+def all(_in, _out, dp=None):
+    if dp:
+        return allWithProgress(_in, _out, dp)
+    return allNoProgress(_in, _out)
+
+def allNoProgress(_in, _out):
+    try:
+        zin = zipfile.ZipFile(_in, 'r')
+        zin.extractall(_out)
+    except Exception as e:
+        print (str(e))
+        return False
+    return True
+
+def allWithProgress(_in, _out, dp):
+    zin = zipfile.ZipFile(_in,  'r')
+    nFiles = float(len(zin.infolist()))
+    count  = 0
+    try:
+        for item in zin.infolist():
+            count += 1
+            update = count / nFiles * 100
+            dp.update(int(update))
+            zin.extract(item, _out)
+    except Exception as e:
+        print (str(e))
+        return False
+
+    return True
+#checkintegrity13122019
