a0eabe521cac188a78ec011c61f3ec7f4805f2d1
vikingsbuild <37523732+vikingsbuild@users.noreply.github.com>
2020-06-12 21:19:54 -0300

    add kamikaze

--- plugin.video.KamiKaze/default.py
+++ plugin.video.KamiKaze/default.py
@@ -44,7 +44,7 @@ if REMOTE_DBG:
         sys.exit(1)
 
 
-addon = xbmcaddon.Addon('plugin.video.Nightwolf')
+addon = xbmcaddon.Addon('plugin.video.KamiKaze')
 addon_version = addon.getAddonInfo('version')
 profile = xbmc.translatePath(addon.getAddonInfo('profile').decode('utf-8'))
 home = xbmc.translatePath(addon.getAddonInfo('path').decode('utf-8'))
